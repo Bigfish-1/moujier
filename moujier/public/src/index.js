61b91a386bb51d1a0a95b9fe8612122d81356b88
某己 <1599725106@qq.com>
2018-12-04 21:34:47 +0800

    moujier homepage

---
+++ moujier/public/src/index.js
@@ -0,0 +1,12 @@
+import test from './test';
+test();
+
+//引入less文件
+import img1 from '../styles/less/index.less';
+
+
+//引入图片
+import '../images/header.jpg';
+
+console.log('aaa');
+console.log(img1);
---
+++ moujier/public/src/test.js
@@ -0,0 +1,3 @@
+export default function fn() {
+    console.log('测试webpack连接, 改变了！');
+}
